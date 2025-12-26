0068f8c76bb6add6ff822127a21a5b6a54ea9a00
BruceRanger <35343177+BruceRanger@users.noreply.github.com>
2025-12-26 15:45:17 -0600

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -306,7 +306,7 @@ async function resolveBoard() {
       // 3) COLLAPSE and PAUSE so you can see rocks drop
       collapse(board);
       renderBoard(ctx, board, { gameOver });
-      await delay(1350);
+      await delay(1000);
 
       // 4) REFILL and PAUSE so you can see new rocks appear
       refill(board);
